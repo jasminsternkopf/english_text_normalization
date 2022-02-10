1344ab745986f3d95de832db29ef7dea023a16c3
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-02-10 15:15:56 +0100

    king names adjustment for I, V and X

--- src/english_text_normalization/adjustments/king_names_normalization.py
+++ src/english_text_normalization/adjustments/king_names_normalization.py
@@ -5,16 +5,18 @@ from english_text_normalization.auxiliary_methods.txt_files_reading import \
     get_list_out_of_txt_file
 
 KING_NUMBER_MAPPINGS_WITHOUT_DOT = [
-  # (re.compile(r" the I[\.st]?(\W)"), " the first"),
+  #(re.compile(r" the Ist(\W)"), r" the first\1"),
   (re.compile(r" the II[\.nd]{0,3}(\W)"), r" the second\1"),
   (re.compile(r" the III[\.rd]{0,3}(\W)"), r" the third\1"),
   (re.compile(r" the IV[\.th]{0,3}(\W)"), r" the fourth\1"),
-  (re.compile(r" the V[\.th]{0,3}(\W)"), r" the fifth\1"),
+  (re.compile(r" the Vth(\W)"), r" the fifth\1"),
+  #(re.compile(r" the V[\.th]{0,3}(\W)"), r" the fifth\1"),
   (re.compile(r" the VI[\.th]{0,3}(\W)"), r" the sixth\1"),
   (re.compile(r" the VII[\.th]{0,3}(\W)"), r" the seventh\1"),
   (re.compile(r" the VIII[\.th]{0,3}(\W)"), r" the eighth\1"),
   (re.compile(r" the IX[\.th]{0,3}(\W)"), r" the ninth\1"),
-  (re.compile(r" the X[\.th]{0,3}(\W)"), r" the tenth\1"),
+  (re.compile(r" the Xth(\W)"), r" the tenth\1"),
+  #(re.compile(r" the X[\.th]{0,3}(\W)"), r" the tenth\1"),
   (re.compile(r" the XI[\.th]{0,3}(\W)"), r" the eleventh\1"),
   (re.compile(r" the XII[\.th]{0,3}(\W)"), r" the twelfth\1"),
   (re.compile(r" the XIII[\.th]{0,3}(\W)"), r" the thirteenth\1"),
@@ -28,21 +30,33 @@ KING_NUMBER_MAPPINGS_WITHOUT_DOT = [
 ]
 
 
-def normalize_king_names_general(text: str, king_names: Iterable[str], max_number: int = 20) -> str:
-  text = add_the_between_king_name_and_roman_numeral(text, king_names)
+def normalize_king_names_general(text: str, king_names: Iterable[str], safe_king_names_for_I_V_and_X: Iterable[str], max_number: int = 20) -> str:
+  text = add_the_between_king_name_and_roman_numeral(
+    text, king_names, safe_king_names_for_I_V_and_X)
   for roman_numeral in KING_NUMBER_MAPPINGS_WITHOUT_DOT[::-1]:
     text = roman_numeral[0].sub(roman_numeral[1], text)
   return text
 
 
-def add_the_between_king_name_and_roman_numeral(text: str, king_names: Iterable[str]) -> str:
+def add_the_between_king_name_and_roman_numeral(text: str, king_names: Iterable[str], safe_king_names_for_I_V_and_X: Iterable[str]) -> str:
   king_names_conc_with_or = "|".join(king_names)
   king_name_plus_roman_numeral = re.compile(rf"({king_names_conc_with_or}) ([XVI]{{2,5}})\.?")
   text = king_name_plus_roman_numeral.sub(r"\1 the \2", text)
+
+  safe_king_names_conc_with_or = "|".join(safe_king_names_for_I_V_and_X)
+  safe_king_name_plus_I = re.compile(rf"({safe_king_names_conc_with_or}) I\.?")
+  text = safe_king_name_plus_I.sub(r"\1 the first", text)
+
+  safe_king_name_plus_V_or_X = re.compile(rf"({safe_king_names_conc_with_or}) ([VX])\.?")
+  text = safe_king_name_plus_V_or_X.sub(r"\1 the \2th", text)
   return text
+
+
+SAFE_KING_NAMES = {"Henry", "Charles", "James", "George", "Edward",
+                   "Richard", "Ferdinand", "William", "Clement", "Louis", "Napoleon", "Philip", "Thibault", "Clement", "Catharine", "Alexander", "Leo", "Henri", "Christian", "Nicholas", "Hermann", "Akbar", "Harry", "Rodolph", "Gregory", "Fred", "Urban", "Karl", "Frederic", "Paul"}
 
 
 def normalize_our_king_names(text: str) -> str:
   king_names = get_list_out_of_txt_file("data/name_corpus.txt")
-  text = normalize_king_names_general(text, king_names)
+  text = normalize_king_names_general(text, king_names, SAFE_KING_NAMES)
   return text
--- tests/test_king_names_normalization.py
+++ tests/test_king_names_normalization.py
@@ -5,72 +5,79 @@ from english_text_normalization.adjustments.king_names_normalization import (
 
 def test_add_the_between_king_name_and_roman_numeral():
   text = "Charles II, Paul IVth and Henry III."
-  res = add_the_between_king_name_and_roman_numeral(text, {"Charles", "Henry"})
+  res = add_the_between_king_name_and_roman_numeral(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the II, Paul IVth and Henry the III"
 
 
 def test_add_the_between_king_name_and_roman_numeral__roman_numeral_last_in_str():
   text = "Henry III."
-  res = add_the_between_king_name_and_roman_numeral(text, {"Charles", "Henry"})
+  res = add_the_between_king_name_and_roman_numeral(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Henry the III"
 
 
 def test_normalize_king_names_general__roman_numeral_last_in_str():
   text = "Henry III.,"
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Henry the third,"
 
 
 def test_normalize_king_names_general__component_test():
   text = "Charles II, Paul IVth and Henry III. were kings."
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the second, Paul IVth and Henry the third were kings."
 
 
 def test_normalize_king_names_general__is_III_recognized_as_three_even_without_dot():
   text = "Charles III "
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the third "
 
 
 def test_normalize_king_names_general__the_was_already_between_name_and_numeral():
   text = "Charles the II."
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the second."
 
 
 def test_normalize_king_names_general__not_kings_name_followed_by_the_and_numeral():
   text = "I read the IX. book"
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "I read the ninth book"
 
 
 def test_normalize_king_names_general__second_with_different_versions():
   text = "Charles the IInd, Charles IId Charles IId. Charles IInd. "
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the second, Charles the second Charles the second Charles the second "
 
 
 def test_normalize_king_names_general__third_with_different_versions():
   text = "Charles the IIId, Charles IIIrd Charles IIIrd. "
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the third, Charles the third Charles the third "
 
 
 def test_normalize_king_names_general__fourth_with_different_versions():
   text = "Charles the IVth, Charles IVth. Charles IV. "
-  res = normalize_king_names_general(text, {"Charles", "Henry"})
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Charles"})
 
   assert res == "Charles the fourth, Charles the fourth Charles the fourth "
+
+
+def test_normalize_king_names_general__king_name_that_is_not_in_safe_king_names_not_replaced_for_I_V_and_X():
+  text = "Charles I., Charles V and Charles X won't be replaced."
+  res = normalize_king_names_general(text, {"Charles", "Henry"}, {"Henry"})
+
+  assert res == text
 
 
 def test_normalize_our_king_names():
@@ -81,7 +88,7 @@ def test_normalize_our_king_names():
 
 
 def test_normalize_our_king_names__without_dot():
-  text = "Charles III and James I, Henry V and Edward VI were kings."
+  text = "Charles III and James I, Henry V, Fred X and Edward VI were kings."
   res = normalize_our_king_names(text)
 
-  assert res == "Charles the third and James the first, Henry the fifth and Edward the sixth were kings."
+  assert res == "Charles the third and James the first, Henry the fifth, Fred the tenth and Edward the sixth were kings."
