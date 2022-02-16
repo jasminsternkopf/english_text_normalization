53b1c96228a6e4531193f2ad6bbae54e2a44c5da
Jasmin Sternkopf <jasmin_sternkopf@web.de>
2022-02-16 15:40:37 +0100

    changes in sentence_extraction

--- src/english_text_normalization/sentence_extraction.py
+++ src/english_text_normalization/sentence_extraction.py
@@ -1,21 +1,16 @@
 import re
 from pathlib import Path
+from typing import Callable
 
-from english_text_normalization.adjustments.abbreviations import \
-    expand_abbreviations
-from english_text_normalization.adjustments.layout_normalization import (
-    remove_linebreaks, remove_repeated_spaces)
-from english_text_normalization.adjustments.remove_dots_that_are_not_end_of_sentence import \
-    remove_dot_after_single_capital_letters
 from english_text_normalization.auxiliary_methods.txt_files_reading import \
     get_text_files
 
 
-def extract_sentences_of_all_books(folder: Path, new_folder: Path) -> str:
+def extract_sentences_of_all_books(folder: Path, new_folder: Path, normalizer: Callable[[str], str]) -> str:
   paths = get_text_files(folder)
   for path in paths:
     book = path.read_text()
-    sentencewise_book = extract_sentences(book)
+    sentencewise_book = extract_sentences(book, normalizer)
     new_path_with_txt_file = new_folder / path.relative_to(folder)
     new_path = new_path_with_txt_file.parent
     if not new_path.exists():
@@ -25,16 +20,12 @@ def extract_sentences_of_all_books(folder: Path, new_folder: Path) -> str:
 
 SENTENCE_ENDS = [".", "?", "!"]
 SENTENCE_ENDS = [re.escape(x) for x in SENTENCE_ENDS]
-SENTENCE_ENDS_AND_CAPITAL_LETTER = [re.compile(
-  rf"({end}\"?)(?: |\-\-|\.\.\.)(\"?[A-Z])") for end in SENTENCE_ENDS]
+#SENTENCE_ENDS_AND_CAPITAL_LETTER = [re.compile(rf"({end}\"?)(?: |\-\-|\.\.\.)(\"?[A-Z])") for end in SENTENCE_ENDS]
+SENTENCE_ENDS_AND_CAPITAL_LETTER = [re.compile(rf"({end}\"?) (?:-- )?(\"?[A-Z])") for end in SENTENCE_ENDS]
 
 
-def extract_sentences(text: str) -> str:
-  text = remove_linebreaks(text)
-  text = expand_abbreviations(text)
-  text = remove_dot_after_single_capital_letters(text)
-  text = remove_repeated_spaces(text)
-  text = text.strip()
+def extract_sentences(text: str, normalizer: Callable[[str], str]) -> str:
+  text = normalizer(text)
   for sentence_end in SENTENCE_ENDS_AND_CAPITAL_LETTER:
     text = sentence_end.sub(r"\1\n\2", text)
   text = remove_quotation_marks_in_line_if_uneven_number_of_them(text)
--- tests/test_sentence_extraction.py
+++ tests/test_sentence_extraction.py
@@ -1,82 +1,59 @@
 from english_text_normalization.sentence_extraction import (
     extract_sentences, remove_quotation_marks_in_line_if_uneven_number_of_them)
 
+
+def normalizer(text: str) -> str:
+  return text
+
+
 def test_extract_sentences__dot():
   text = "Hello World. It's a pleasure."
-  res = extract_sentences(text)
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "Hello World.\nIt's a pleasure."
 
 
 def test_extract_sentences__exclamation_mark():
   text = "Hello World! It's a pleasure."
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "Hello World!\nIt's a pleasure."
 
 
 def test_extract_sentences__question_mark():
   text = "Hello World? It's a pleasure."
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "Hello World?\nIt's a pleasure."
 
 
 def test_extract_sentences__quotation_marks_and_dot():
   text = "\"Hello World.\" she said. It's a pleasure."
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "\"Hello World.\" she said.\nIt's a pleasure."
 
 
 def test_extract_sentences__quotation_marks_and_exclamation_mark():
   text = "\"Hello World!\" she said. It's a pleasure."
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "\"Hello World!\" she said.\nIt's a pleasure."
 
 
 def test_extract_sentences__quotation_marks_and_question_mark():
   text = "\"Hello World?\" she asked. It's a pleasure."
-  res = extract_sentences(text)
+  res = extract_sentences(text, normalizer)
 
   assert res == "\"Hello World?\" she asked.\nIt's a pleasure."
 
 
-def test_extract_sentences__contains_abbreviation():
-  text = "Hello Doctor Doe, how are you?"
-  res = extract_sentences(text)
-
-  assert res == text
-
-
-def test_extract_sentences__contains_single_letters_with_dot():
-  text = "What time is it? \"It's 3 P.M., Sir.\""
-  res = extract_sentences(text)
-
-  assert res == "What time is it?\n\"It's 3 P M, Sir.\""
-
-
-def test_extract_sentences__double_hyphen_between_sentences():
-  text = "Hello World!--You look good."
-  res = extract_sentences(text)
+def test_extract_sentences__space_double_hyphen_space_between_sentences():
+  text = "Hello World! -- You look good."
+  res = extract_sentences(text, normalizer)
 
   assert res == "Hello World!\nYou look good."
 
-
-def test_extract_sentences__three_dots_between_sentences():
-  text = "Hello World!...You look good."
-  res = extract_sentences(text)
-
-  assert res == "Hello World!\nYou look good."
-
-
-def test_extract_sentences__three_dots_between_sentences_and_the_first_one_ends_with_dot():
-  text = "Hello World....You look good."
-  res = extract_sentences(text)
-
-  assert res == "Hello World.\nYou look good."
 
 
 def test_remove_quotation_marks_in_line_if_uneven_number_of_them():
